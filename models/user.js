bd2d7c9805d1355d5ca6901040cc62ba9362e85d
TrevorThomp <trevor.ray.thompson@gmail.com>
2019-12-12 14:13:00 -0800

    added hash function to encrypt user passwords

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -21,6 +22,16 @@ const userSchema = new mongoose.Schema({
     required: true
   }
 })
-
+// hash password before saving to database
+userSchema.pre('save', function(next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, function(err,hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+})
 const User = mongoose.model('User', userSchema);
 module.exports = User;
